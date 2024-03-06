31c7a6fb9d74ae225b6920a0ecd03a704d6ceeb1
Nilesh410 <shirudenileshs@gmail.com>
2024-03-06 18:03:01 +0530

    Add selection sort using JAVA

---
+++ Sorting_Algorithm/Selection_Sort/selectionsort.java
@@ -0,0 +1,50 @@
+package Sorting_Algorithm.Selection_Sort;
+
+/**
+ * selectionsort
+ */
+public class selectionsort {
+
+    public static void main(String[] args) {
+       int arr[]={9,6,2,1,4};
+       System.out.println("Array before sorted=");
+       for(int i:arr)
+       {
+        System.out.println(i);
+       }
+       selectionsort1(arr);
+       System.out.println("Sorted elements are=");
+       for(int i:arr)
+       {
+        System.out.println(i);
+       }
+       
+
+    }
+    public static void selectionsort1(int arr[])
+    {
+        for(int i=0;i<arr.length-1;i++)
+        {
+            int index_small_ele=smallindexele(arr,i);
+            if(i!=index_small_ele)
+            {
+                int temp=arr[i];
+                arr[i]=arr[index_small_ele];
+                arr[index_small_ele]=temp;
+            }
+            
+        }
+    }
+    public static int smallindexele(int arr[],int i)
+    {
+        int small_index=i;
+        for(int j=i+1;j<arr.length;j++)
+        {
+            if(arr[small_index]>arr[j])
+            {
+                small_index=j;
+            }
+        }
+        return small_index;
+    }
+}
